2b66aeda8191ddb727a27ca575158f1c0bb7abb9
Jørgen Hauge <johauge@cisco.com>
2017-11-22 10:59:41 +0100

    Updates to device_regex. Adding regex_fix for parsing forwarded devices as well.

--- adbpy/devices.py
+++ adbpy/devices.py
@@ -1,16 +1,22 @@
 import re
 
-device_regex = re.compile(r'(\w+)\t(device|offline|unauthorized)')
-forward_regex = re.compile(
-    r'^(?P<device_id>\w+)?\s+tcp:(?P<local_port>\d+)?\s+tcp:(?P<remote_port>\d+)')
+# device_regex = re.compile(r'(\w+)\t(device|offline|unauthorized)')
+device_regex = re.compile(r'(?P<device_id>\w+)\s+(?P<device_status>device|offline|unauthorized)\s+?(usb:(?P<usb>[a-zA-Z0-9\-\.]+)\s+product:(?P<product>\w+)\s+model:(?P<model>\w+)\s+device:(?P<device>\w+))?')
+forward_regex = re.compile(r'^(?P<device_id>\w+)?\s+tcp:(?P<local_port>\d+)?\s+tcp:(?P<remote_port>\d+)')
 
 
-def parse_device_list(device_list):
-    return device_regex.findall(device_list)
+def parse_string(string, regex):
+    _devices = {}
+    for device in re.finditer(regex, string):
+        group_dict = device.groupdict()
+        device_id = group_dict.pop('device_id')
+        _devices[device_id] = group_dict
+    return _devices
 
 
-def parse_forward_list(forwarded_list):
-    _forwarded_devices = {}
-    for device in re.finditer(forward_regex, forwarded_list):
-        _forwarded_devices[device.pop('device_id')] = device
-    return _forwarded_devices
+def parse_forward_string(forwarded_string):
+    return parse_string(forwarded_string, forward_regex)
+
+
+def parse_device_string(device_string):
+    return parse_string(device_string, device_regex)
