8957e94d211b752f328ed1ae36dfbfba0c950039
Jorgen <jorgen78@gmail.com>
2017-11-24 21:50:09 +0100

    Adding tests for checking output of parser for forwarding string.

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -1,4 +1,4 @@
-from adbpy.devices import parse_device_string
+from adbpy.devices import parse_device_string, parse_forward_string
 
 
 def test_parse_device():
@@ -17,7 +17,19 @@ def test_parse_device():
     assert parse_device_string(input) == expected
 
     input = "1ccb04bb\tdevice\n950a8ad5\tunauthorized\n1234567\toffline\n"
-    expected =  {"1ccb04bb": {"device": None, "device_status": "device", "model": None, "product": None, "usb": None},
-        "950a8ad5": {"device": None, "device_status": "unauthorized", "model": None, "product": None, "usb": None},
-        "1234567": {"device": None, "device_status": "offline", "model": None, "product": None, "usb": None}}
+    expected = {"1ccb04bb": {"device": None, "device_status": "device", "model": None, "product": None, "usb": None},
+                "950a8ad5": {"device": None, "device_status": "unauthorized", "model": None, "product": None, "usb": None},
+                "1234567": {"device": None, "device_status": "offline", "model": None, "product": None, "usb": None}}
     assert parse_device_string(input) == expected
+
+
+def test_parse_forward():
+    input = "1ccb04bb\ntcp:4\ntcp:2"
+    expected = {"1ccb04bb": {"local_port": "4", "remote_port": "2"}}
+    assert parse_forward_string(input) == expected
+
+    input = "1ccb04bb\ntcp:4\ntcp:2\n950a8ad5\ntcp:4\ntcp:3\n1234567\ntcp:4\ntcp:5"
+    expected = {"1ccb04bb": {"local_port": "4", "remote_port": "2"},
+                "950a8ad5": {"local_port": "4", "remote_port": "3"},
+                "1234567": {"local_port": "4", "remote_port": "5"}}
+    assert parse_forward_string(input) == expected
