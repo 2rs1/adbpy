5b23e9051713307ada33ccde8750aed37d998df9
Jorgen <jorgen78@gmail.com>
2017-11-24 21:48:57 +0100

    PEP8

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -8,8 +8,8 @@ def test_parse_device():
     assert parse_device_string(input) == expected
 
     input = "1ccb04bb\tdevice\n950a8ad5\tunauthorized\n"
-    expected =  {"1ccb04bb": {"device": None, "device_status": "device", "model": None, "product": None, "usb": None},
-        "950a8ad5": {"device": None, "device_status": "unauthorized", "model": None, "product": None, "usb": None}}
+    expected = {"1ccb04bb": {"device": None, "device_status": "device", "model": None, "product": None, "usb": None},
+                "950a8ad5": {"device": None, "device_status": "unauthorized", "model": None, "product": None, "usb": None}}
     assert parse_device_string(input) == expected
 
     input = ""
