f73cdc890668acd4aff83c23a325e7672a75f940
Jørgen Hauge <johauge@cisco.com>
2017-11-24 16:52:14 +0100

    Fix so that it doesn't have to start with(^).

--- adbpy/devices.py
+++ adbpy/devices.py
@@ -1,7 +1,7 @@
 import re
 
 device_regex = re.compile(r'(?P<device_id>\w+)\s+(?P<device_status>device|offline|unauthorized)\s+?(usb:(?P<usb>[a-zA-Z0-9\-\.]+)\s+product:(?P<product>\w+)\s+model:(?P<model>\w+)\s+device:(?P<device>\w+))?')
-forward_regex = re.compile(r'^(?P<device_id>\w+)?\s+tcp:(?P<local_port>\d+)?\s+tcp:(?P<remote_port>\d+)')
+forward_regex = re.compile(r'(?P<device_id>\w+)?\s+tcp:(?P<local_port>\d+)?\s+tcp:(?P<remote_port>\d+)')
 
 
 def parse_string(string, regex):
